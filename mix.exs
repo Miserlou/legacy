21eaacae7a69bcb85f9ba827efadbe61ee9056ff
Rich Jones <rich@anomos.info>
2016-03-11 20:50:35 +0100

    mix exs

---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule Legacy.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :legacy,
+     version: "0.1.3",
+     elixir: "~> 1.0",
+     deps: deps,
+     package: [
+       licenses: ["MIT"],
+     ],
+     description: """
+     Elixir Legacy Client
+     """]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger, :inets, :ssl]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [{:exvcr, "~> 0.4.0", only: [:test]},
+     {:poison, "~> 1.4"}
+    ]
+  end
+end
